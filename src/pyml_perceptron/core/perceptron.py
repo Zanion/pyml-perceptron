d78d1acb2e864de1d842cc190f2b2cb0fab2166b
Dustin Kerns <Dkerns0123@gmail.com>
2020-03-09 20:43:24 -0700

    Fix netinput computation

--- src/pyml_perceptron/core/perceptron.py
+++ src/pyml_perceptron/core/perceptron.py
@@ -54,7 +54,7 @@ class Perceptron:
 
     def net_input(self, X):
         """ Calculate net input """
-        return np.dot(X, self.w_[1:] + self.w_[0])
+        return np.dot(X, self.w_[1:]) + self.w_[0]
 
 
     def predict(self, X):
