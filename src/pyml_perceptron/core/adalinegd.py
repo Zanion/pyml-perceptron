f22ce7e3f2771e9c1132e6892f5784c3c71c6421
Dustin Kerns <Dkerns0123@gmail.com>
2020-03-09 21:03:31 -0700

    Add Adaline notebook

--- src/pyml_perceptron/core/adalinegd.py
+++ src/pyml_perceptron/core/adalinegd.py
@@ -38,7 +38,7 @@ class AdalineGD:
         rgen = np.random.RandomState(self.random_state)
         # Initialize weights to non-zero values
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
-        self.cost_ []
+        self.cost_ = []
 
         for _ in range(self.n_iter):
             net_input = self.net_input(X)
